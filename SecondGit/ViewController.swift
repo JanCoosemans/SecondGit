bf0e821ba1dc8c5e9bffbe9d061ab4885c65bf15
JanCoosemans <Jan_Matei_Coosemans@outlook.com>
2019-07-29 23:39:42 +0200

    Initial Commit

---
+++ SecondGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecondGit
+//
+//  Created by Jan Coosemans on 29/07/2019.
+//  Copyright © 2019 Jan Coosemans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
